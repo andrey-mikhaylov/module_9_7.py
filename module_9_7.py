c899e41673a7b737f54cdf16c54a2b3d9c5e94ff
Andrey <andrey.mikhaylov.n@gmail.com>
2024-10-20 01:10:18 +0300

    done

--- module_9_7.py
+++ module_9_7.py
@@ -1,3 +1,72 @@
+from math import sqrt
+
+Number = int | float
+
+
+def prime_check(x: int) -> bool:
+    if x < 2:
+        return False
+
+    for i in range(2, int(sqrt(x)) + 1):
+        if x % i == 0:
+            return False
+
+    return True
+
+
+def is_prime(func):
+    """
+    Функция декоратор, которая распечатывает
+    "Простое", если результат 1ой функции будет простым числом
+    и "Составное" в противном случае.
+    :param func: функция
+    :return:
+    """
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        print("Простое" if prime_check(result) else "Составное")
+        return result
+    """
+    @is_prime - декоратор для функции sum_three
+    """
+    return wrapper
+
+
+@is_prime
+def sum_three(a: Number, b: Number, c: Number) -> Number:
+    """
+    Складывает 3 числа
+    :param b: число
+    :param a: число
+    :param c: число
+    :return: сумма a+b+c
+    """
+    return a + b + c
+
+
+def test1():
+    for i in range(20):
+        print(is_prime(lambda x: x)(i))
+
+    result = sum_three(12, 0, 0)
+    print(result)
+
+
+def test():
+    result = sum_three(2, 3, 6)
+    print(result)
+    """
+    Результат консоли:
+    Простое
+    11
+    """
+
+
+if __name__ == '__main__':
+    test1()
+    test()
+
+
 """
 2023/12/05 00:00|Домашнее задание по теме "Декораторы"
 Задание: Декораторы в Python
